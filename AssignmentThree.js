a0d1be22551493af3692967e64a4ddd459ee8997
Maxwell <maxperryg@gmail.com>
2018-12-17 16:38:14 -0500

    commented out more

--- AssignmentThree.js
+++ AssignmentThree.js
@@ -84,184 +84,184 @@ function start4(){
     if(isClassful) startClassful(classLetter);
     if(!isClassful) startClassless();
 }
-function startClassful(letter){
-    var output = document.getElementById("output");
-    switch (letter) {
-        case "A":
-            leadingBits = '0';
-            prefixSize = 8;
-            break;
-        case "B":
-            leadingBits = '10';
-            prefixSize = 16;
-            break;
-        case "C":
-            leadingBits = '110';
-            prefixSize = 24;
-            break;
-        case "D":
-            leadingBits = '1110';
-            prefixSize = 32;
-        case "E":
-            output.innerHTML = "<h1>Reserved</h1>";
-            return;
-    }
-    var table = document.getElementById("outputTable");
-    table.innerHTML += ('<tr> <th>Number</th>\n' +
-        '    <th>IP Address in Dotted Decimal Notation</th>\n' +
-        '    <th>IP Address in Dotted Binary Notation</th> </tr>');
-    if (isClassful) binaryNetwork = networkNumberFromPrefix(leadingBits, prefixSize);
-    for (var i=1; i<=amount; i++){
-        var address;
-        if (!isClassful) {
-            var array = binaryNetwork.slice();
-            array[array.length - 1] = padOctet(array[array.length - 1]);
-            address = addAddressToNetwork(array);
-        }
-        else address = addAddressToNetwork(binaryNetwork);
-        dottedDecimal = binToDottedDec(address);
-        binary = binaryArrayToDotted(address);
-        table.innerHTML += ('<tr>\n' +
-            '    <td>' + i + '</td>\n' +
-            '    <td>' + dottedDecimal + '</td>\n' +
-            '    <td>' + binary + '</td>\n' +
-            '  </tr>');
-        if (classLetter === "D") break;
-    }
-}
-function startClassless(){
-    var bitsNeededInHost = Math.ceil(Math.log2(amount));
-    prefixSize = 32 - bitsNeededInHost;
-    for (var i=leadingBits.length; i < prefixSize; i++){
-        leadingBits += randomBit();
-    }
-        // if leading bits are more than 1 octet length, push each octet into network array
-    while (leadingBits.length > 8) {
-        binaryNetwork.push(leadingBits.substring(0, 8));
-        leadingBits = leadingBits.substring(8);
-    } // push remaining bits into network array
-    if (leadingBits) binaryNetwork.push(leadingBits);
-    var table = document.getElementById("outputTable");
-    table.innerHTML += ('<tr> <th>Number</th>\n' +
-        '    <th>IP Address in Dotted Decimal Notation</th>\n' +
-        '    <th>IP Address in Dotted Binary Notation</th> </tr>');
-    if (isClassful) binaryNetwork = networkNumberFromPrefix(leadingBits, prefixSize);
-    for (var i=1; i<=amount; i++){
-        var address;
-        if (!isClassful) {
-            var array = binaryNetwork.slice();
-            array[array.length - 1] = padOctet(array[array.length - 1]);
-            address = addAddressToNetwork(array);
-        }
-        else address = addAddressToNetwork(binaryNetwork);
-        dottedDecimal = binToDottedDec(address);
-        binary = binaryArrayToDotted(address);
-        table.innerHTML += ('<tr>\n' +
-            '    <td>' + i + '</td>\n' +
-            '    <td>' + dottedDecimal + '</td>\n' +
-            '    <td>' + binary + '</td>\n' +
-            '  </tr>');
-        if (classLetter === "D") break;
-    }
-}
-function randomBit() {
-    return Math.round(Math.random());
-}
-
-function randomOctet() {
-    var octet = '';
-    for (var i=0; i<8; i++) octet += randomBit();
-    return octet;
-}
-
-function padOctet(octet) {
-    while (octet.length < 8){
-        octet += randomBit();
-    }
-    return octet;
-}
-
-function networkNumberFromPrefix (leadingBits, size) {
-    var array=[];
-    // get first octet
-    array.push(padOctet(leadingBits));
-    // get remaining octets
-    for (var i = 1; i < size; i+=8)
-        array.push(randomOctet());
-    return array;
-}
-
-
-function addAddressToNetwork (network) {
-    var array = network.slice();
-    for(var i=array.length; i<4; i++){
-        array.push(randomOctet());
-    }
-    return array;
-}
-
-function binToDottedDec(binaryArray) {
-    var output = "";
-    for (var i=0; i<binaryArray.length; i++){
-        output += binToDec(binaryArray[i]);
-        if (i+ 1<binaryArray.length) output += '.';
-    }
-    return output;
-}
-
-function binToColonHex(binArray, shouldCompress) {
-    var output = "";
-    for (var i=0; i<binArray.length; i += 2){
-        var number = decimalToHex(binToDec((binArray[i] + binArray[i+1])));
-        if (!(number === '0' && shouldCompress)) output += number;
-        if (i+2 < binArray.length) output += ':';
-    }
-    return output;
-}
-
-function binToDec(binary) {
-    return parseInt((binary + '')
-        .replace(/[^01]/gi, ''), 2);
-}
-function decToBin(decimal) {
-    return parseInt(decimal, 10).toString(2);
-}
-
-function binaryArrayToDotted (binaryArray) {
-    var output = "";
-    for (var i=0; i<binaryArray.length; i++){
-        output += (binaryArray[i]);
-        if (i+ 1<binaryArray.length) output += '.';
-    }
-    return output;
-}
-
-function decimalToHex(d) {
-    return Number(d).toString(16);
-}
-function handleIpv6(typeNeeded) {
-    let module = document.getElementById('type-needed'),
-        table = document.getElementById('output');
-    module.innerHTML = '<h1>You asked for ' + numberOfNeededAddresses + ' '
-        + typeNeeded + ' addresses.</h1>';
-    table.innerHTML += ('<tr> <th>Number</th>\n' +
-        '    <th>IP Address in Colon Hex Notation</th>\n' +
-        '    <th>IP Address in Dotted Binary Notation</th> </tr>');
-    let binNetwork = [];
-    for (let i= 0; i<8; i++){
-        binNetwork.push(randomOctet());
-    }
-
-    for (let i=1; i<=numberOfNeededAddresses; i++){
-        // console.log(binNetwork);
-        let host = binNetwork.slice();
-        for (let i= 0; i<8; i++) {
-            host.push(randomOctet());
-        }
-        table.innerHTML += ('<tr>\n' +
-            '    <td>' + i + '</td>\n' +
-            '    <td>' + binToColonHex(host, !typeNeeded.includes('without')) + '</td>\n' +
-            '    <td>' + binaryArrayToDotted(host) + '</td>\n' +
-            '  </tr>');
-    }
-}
+//function startClassful(letter){
+//    var output = document.getElementById("output");
+//    switch (letter) {
+//        case "A":
+//            leadingBits = '0';
+//            prefixSize = 8;
+//            break;
+//        case "B":
+//            leadingBits = '10';
+//            prefixSize = 16;
+//            break;
+//        case "C":
+//            leadingBits = '110';
+//            prefixSize = 24;
+//            break;
+//        case "D":
+//            leadingBits = '1110';
+//            prefixSize = 32;
+//        case "E":
+//            output.innerHTML = "<h1>Reserved</h1>";
+//            return;
+//    }
+//    var table = document.getElementById("outputTable");
+//    table.innerHTML += ('<tr> <th>Number</th>\n' +
+//        '    <th>IP Address in Dotted Decimal Notation</th>\n' +
+//        '    <th>IP Address in Dotted Binary Notation</th> </tr>');
+//    if (isClassful) binaryNetwork = networkNumberFromPrefix(leadingBits, prefixSize);
+//    for (var i=1; i<=amount; i++){
+//        var address;
+//        if (!isClassful) {
+//            var array = binaryNetwork.slice();
+//            array[array.length - 1] = padOctet(array[array.length - 1]);
+//            address = addAddressToNetwork(array);
+//        }
+//        else address = addAddressToNetwork(binaryNetwork);
+//        dottedDecimal = binToDottedDec(address);
+//        binary = binaryArrayToDotted(address);
+//        table.innerHTML += ('<tr>\n' +
+//            '    <td>' + i + '</td>\n' +
+//            '    <td>' + dottedDecimal + '</td>\n' +
+//            '    <td>' + binary + '</td>\n' +
+//            '  </tr>');
+//        if (classLetter === "D") break;
+//    }
+//}
+//function startClassless(){
+//    var bitsNeededInHost = Math.ceil(Math.log2(amount));
+//    prefixSize = 32 - bitsNeededInHost;
+//    for (var i=leadingBits.length; i < prefixSize; i++){
+//        leadingBits += randomBit();
+//    }
+//        // if leading bits are more than 1 octet length, push each octet into network array
+//    while (leadingBits.length > 8) {
+//        binaryNetwork.push(leadingBits.substring(0, 8));
+//        leadingBits = leadingBits.substring(8);
+//    } // push remaining bits into network array
+//    if (leadingBits) binaryNetwork.push(leadingBits);
+//    var table = document.getElementById("outputTable");
+//    table.innerHTML += ('<tr> <th>Number</th>\n' +
+//        '    <th>IP Address in Dotted Decimal Notation</th>\n' +
+//        '    <th>IP Address in Dotted Binary Notation</th> </tr>');
+//    if (isClassful) binaryNetwork = networkNumberFromPrefix(leadingBits, prefixSize);
+//    for (var i=1; i<=amount; i++){
+//        var address;
+//        if (!isClassful) {
+//            var array = binaryNetwork.slice();
+//            array[array.length - 1] = padOctet(array[array.length - 1]);
+//            address = addAddressToNetwork(array);
+//        }
+//        else address = addAddressToNetwork(binaryNetwork);
+//        dottedDecimal = binToDottedDec(address);
+//        binary = binaryArrayToDotted(address);
+//        table.innerHTML += ('<tr>\n' +
+//            '    <td>' + i + '</td>\n' +
+//            '    <td>' + dottedDecimal + '</td>\n' +
+//            '    <td>' + binary + '</td>\n' +
+//            '  </tr>');
+//        if (classLetter === "D") break;
+//    }
+//}
+//function randomBit() {
+//    return Math.round(Math.random());
+//}
+//
+//function randomOctet() {
+//    var octet = '';
+//    for (var i=0; i<8; i++) octet += randomBit();
+//    return octet;
+//}
+//
+//function padOctet(octet) {
+//    while (octet.length < 8){
+//        octet += randomBit();
+//    }
+//    return octet;
+//}
+//
+//function networkNumberFromPrefix (leadingBits, size) {
+//    var array=[];
+//    // get first octet
+//    array.push(padOctet(leadingBits));
+//    // get remaining octets
+//    for (var i = 1; i < size; i+=8)
+//        array.push(randomOctet());
+//    return array;
+//}
+//
+//
+//function addAddressToNetwork (network) {
+//    var array = network.slice();
+//    for(var i=array.length; i<4; i++){
+//        array.push(randomOctet());
+//    }
+//    return array;
+//}
+//
+//function binToDottedDec(binaryArray) {
+//    var output = "";
+//    for (var i=0; i<binaryArray.length; i++){
+//        output += binToDec(binaryArray[i]);
+//        if (i+ 1<binaryArray.length) output += '.';
+//    }
+//    return output;
+//}
+//
+//function binToColonHex(binArray, shouldCompress) {
+//    var output = "";
+//    for (var i=0; i<binArray.length; i += 2){
+//        var number = decimalToHex(binToDec((binArray[i] + binArray[i+1])));
+//        if (!(number === '0' && shouldCompress)) output += number;
+//        if (i+2 < binArray.length) output += ':';
+//    }
+//    return output;
+//}
+//
+//function binToDec(binary) {
+//    return parseInt((binary + '')
+//        .replace(/[^01]/gi, ''), 2);
+//}
+//function decToBin(decimal) {
+//    return parseInt(decimal, 10).toString(2);
+//}
+//
+//function binaryArrayToDotted (binaryArray) {
+//    var output = "";
+//    for (var i=0; i<binaryArray.length; i++){
+//        output += (binaryArray[i]);
+//        if (i+ 1<binaryArray.length) output += '.';
+//    }
+//    return output;
+//}
+//
+//function decimalToHex(d) {
+//    return Number(d).toString(16);
+//}
+//function handleIpv6(typeNeeded) {
+//    let module = document.getElementById('type-needed'),
+//        table = document.getElementById('output');
+//    module.innerHTML = '<h1>You asked for ' + numberOfNeededAddresses + ' '
+//        + typeNeeded + ' addresses.</h1>';
+//    table.innerHTML += ('<tr> <th>Number</th>\n' +
+//        '    <th>IP Address in Colon Hex Notation</th>\n' +
+//        '    <th>IP Address in Dotted Binary Notation</th> </tr>');
+//    let binNetwork = [];
+//    for (let i= 0; i<8; i++){
+//        binNetwork.push(randomOctet());
+//    }
+//
+//    for (let i=1; i<=numberOfNeededAddresses; i++){
+//        // console.log(binNetwork);
+//        let host = binNetwork.slice();
+//        for (let i= 0; i<8; i++) {
+//            host.push(randomOctet());
+//        }
+//        table.innerHTML += ('<tr>\n' +
+//            '    <td>' + i + '</td>\n' +
+//            '    <td>' + binToColonHex(host, !typeNeeded.includes('without')) + '</td>\n' +
+//            '    <td>' + binaryArrayToDotted(host) + '</td>\n' +
+//            '  </tr>');
+//    }
+//}
