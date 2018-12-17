32b564902e72ae69fbd332ca80afa5607044eb02
Maxwell <maxperryg@gmail.com>
2018-12-17 16:37:15 -0500

    commented out

--- AssignmentThree.js
+++ AssignmentThree.js
@@ -164,14 +164,6 @@ function startClassless(){
         if (classLetter === "D") break;
     }
 }
-//function isIpv4 (input) {
-//    return ipv4Format.test(input);
-//}
-//
-//function isIpv6 (input) {
-//    return ipv6Format.test(input);
-//}
-
 function randomBit() {
     return Math.round(Math.random());
 }
@@ -247,92 +239,6 @@ function binaryArrayToDotted (binaryArray) {
 function decimalToHex(d) {
     return Number(d).toString(16);
 }
-
-//function handleIpv4(typeNeeded) {
-//    var module = document.getElementById('type-needed'),
-//        leadingBits,
-//        table = document.getElementById('output');
-//        let classless;
-//    module.innerHTML = '<h1>You asked for ' + numberOfNeededAddresses + ' '
-//        + typeNeeded + ' addresses.</h1>';
-//
-//    if (typeNeeded === "ipv4 classless") {
-//        classless = true;
-//        if (numberOfNeededAddresses < 256) typeNeeded = "ipv4 C";
-//        else if (numberOfNeededAddresses < 65536) typeNeeded = "ipv4 B";
-//        else if (numberOfNeededAddresses < 16777216) typeNeeded = "ipv4 A";
-//    }
-////    switch (typeNeeded) {
-////        case "ipv4 A":
-////            if (numberOfNeededAddresses > 16777216){
-////                module.innerHTML = "<h1>Sorry, Class A addressing supports up to 16,777,216 addresses per network, and you requested " + numberOfNeededAddresses + "</h1>";
-////                return;
-////            }
-////            leadingBits = '0';
-////            prefixSize = 8;
-////            break;
-////        case "ipv4 B":
-////            if (numberOfNeededAddresses > 65536){
-////                module.innerHTML = "<h1>Sorry, Class B addressing supports up to 65,536 addresses per network, and you requested " + numberOfNeededAddresses + "</h1>";
-////                return;
-////            }
-////            leadingBits = '10';
-////            prefixSize = 16;
-////            break;
-////        case "ipv4 C":
-////            if (numberOfNeededAddresses > 256){
-////                module.innerHTML = "<h1>Sorry, Class C addressing supports up to 256 addresses per network, and you requested " + numberOfNeededAddresses + "</h1>";
-////                return;
-////            }
-////            leadingBits = '110';
-////            prefixSize = 24;
-////            break;
-////        case "ipv4 D":
-////            leadingBits = '1110';
-////            prefixSize = 32;
-////            module.innerHTML = '<h1>You asked for an IP address for multicast addressing.</h1>';
-////            break;
-////        case "ipv4 E":
-////            module.innerHTML = "<h1>Sorry, such addresses are \“reserved\”</h1>";
-////            return;
-////    }
-//    if (classless) {
-//        let bitsNeededInHost = Math.ceil(Math.log2(numberOfNeededAddresses));
-//        prefixSize = 32 - bitsNeededInHost;
-//        for (let i=leadingBits.length; i < prefixSize; i++){
-//            leadingBits += randomBit();
-//        }
-//        // if leading bits are more than 1 octet length, push each octet into network array
-//        while (leadingBits.length > 8) {
-//            binaryNetwork.push(leadingBits.substring(0, 8));
-//            leadingBits = leadingBits.substring(8);
-//        } // push remaining bits into network array
-//        if (leadingBits) binaryNetwork.push(leadingBits);
-//    }
-//
-//    table.innerHTML += ('<tr> <th>Number</th>\n' +
-//        '    <th>IP Address in Dotted Decimal Notation</th>\n' +
-//        '    <th>IP Address in Dotted Binary Notation</th> </tr>');
-//    if (!classless) binaryNetwork = networkNumberFromPrefix(leadingBits, prefixSize);
-//    for (let i=1; i<=numberOfNeededAddresses; i++){
-//        let address;
-//        if (classless) {
-//            let array = binaryNetwork.slice();
-//            array[array.length - 1] = padOctet(array[array.length - 1]);
-//            address = addAddressToNetwork(array);
-//        }
-//        else address = addAddressToNetwork(binaryNetwork);
-//        dottedDecimal = binToDottedDec(address);
-//        binary = binaryArrayToDotted(address);
-//        table.innerHTML += ('<tr>\n' +
-//            '    <td>' + i + '</td>\n' +
-//            '    <td>' + dottedDecimal + '</td>\n' +
-//            '    <td>' + binary + '</td>\n' +
-//            '  </tr>');
-//        if (typeNeeded === "ipv4 D") break;
-//    }
-//}
-
 function handleIpv6(typeNeeded) {
     let module = document.getElementById('type-needed'),
         table = document.getElementById('output');
